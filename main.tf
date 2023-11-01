9b966638bd935a19019283200a34ae9425fbe4e1
vbavalatti <vijay.nb@gmail.com>
2023-11-01 07:23:44 +0530

    update03

---
+++ main.tf
@@ -0,0 +1,14 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "Vijay-test-01"
+
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
